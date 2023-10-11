fb60d0a3f0340de365c33730dd00b370e1e4fefb
유준호 <juno@yujunhoui-MacBookPro.local>
2023-10-11 23:23:40 +0900

    231011

---
+++ boj14501.cpp
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/14501
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int N;
+    cin>>N;
+
+    int* T = new int[N];
+    int* P = new int[N];
+    float* V = new float[N];
+    
+
+    for(int i=0 ; i<N ; i++){
+        cin >> T[i] >> P[i];
+        // V[i] = P[i]/T[i];
+    }
+
+    for(int i=0 ; i<N ; i++){
+
+    }
+
+
+
+
+    return 0;
+}
