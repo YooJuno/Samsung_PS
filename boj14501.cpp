a613451952098e0af0250b822a7c35ff0cca5bb5
유준호 <juno@yujunhoui-MacBookPro.local>
2023-10-11 23:51:34 +0900

    231011

--- boj14501.cpp
+++ boj14501.cpp
@@ -1,5 +1,14 @@
 // https://www.acmicpc.net/problem/14501
 
+
+/*
+[생각정리]
+회의실 배정 문제에 가중치를 더한 문제 같음.
+dp를 써야 하는데 감이 안잡힘.
+knapsack problem과 비슷하게 풀어야 할까?
+*/
+
+
 #include <iostream>
 
 using namespace std;
