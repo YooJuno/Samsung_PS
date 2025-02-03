0f1f0520a9794c3c2e08b76be2135dab47bc2194
YooJuno <juno980220@naver.com>
2025-02-03 17:50:03 +0900

    B형 대비

---
+++ B형특강/10726.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+auto main() -> int
+{
+    int TC; cin >> TC;
+    for(auto test_case=1; test_case<=TC; test_case++)
+    {
+        int N, M;
+        cin >> N >> M;
+
+        
+        
+    }
+    return 0;
+}
