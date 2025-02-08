30cc75d3f86a9f7287a514b659174d00527e8ec4
YooJuno <juno980220@naver.com>
2025-02-08 11:37:56 +0900

    directory 이름 변경

--- SWEA/5644-무선충전/5644.cpp
+++ SWEA/5644-무선충전/5644.cpp
@@ -2,34 +2,14 @@
 
 using namespace std;
 
-struct Cell
-{
-    int y;
-    int x;
-};      
-
-int main(int argc, const char** argv) 
+auto main() -> int
 {
     int T; cin >> T;
-    for (int test_case = 0; test_case < T; test_case++)
+    for(auto test_case=1; test_case<=T; test_case++)
     {
-        int sum = 0;
-
-        int M; cin >> M;
-        int A; cin >> A;
-
-        int routeA[M];
-        int routeB[M];
-
-        for (int i = 0; i < M; i++)
-            cin >> routeA[i];
-        for (int i = 0; i < M; i++)
-            cin >> routeB[i];
-
-        
-
-
-        std::cout << sum << std::endl;
+    
+        cout << '#' << test_case << ' ' << result;
     }
+    
     return 0;
 }
