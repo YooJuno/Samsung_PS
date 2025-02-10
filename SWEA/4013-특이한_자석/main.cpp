d86796b2abaa77487b164ff0110889c72de3bd9c
YooJuno <juno980220@naver.com>
2025-02-10 23:09:21 +0900

    문제 푸는 중

--- SWEA/4013-특이한_자석/main.cpp
+++ SWEA/4013-특이한_자석/main.cpp
@@ -17,6 +17,26 @@ struct Magnet
 bool polarities[4][8]; // 날 정보(0:N, 1:S)
 Magnet rotations[21];
 
+void recursive(int srcNum, int idx)
+{
+    // 현재 자석의 번호가 맨 왼쪽이면 왼쪽 분기는 안함
+    if(srcNum) recursive(idx-1);
+    // 현재 자석의 번호가 맨 오른이면 오른쪽 분기는 안함
+    if(idx != 4) recursive(idx+1);
+}
+
+void RotateMagent(int direction, bool magnet[8])
+{
+    if(direction == CLOCKWISE)
+    {
+
+    }
+    else if(direction == COUNTER_CLOCKWISE)
+    {
+        
+    }
+}
+
 auto main() -> int
 {
     freopen("sample_input.txt", "r", stdin);
@@ -26,6 +46,7 @@ auto main() -> int
     {
         int K; cin >> K; // 자석을 회전시키는 횟수
         memset(polarities, 0, sizeof(polarities));
+
         for(auto i = 0; i < 4; i++)
         {
             for(auto j = 0; j < 8; j++)
