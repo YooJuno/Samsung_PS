216313182c88d8e36bbd57856bfff91d3551ac11
rywls1110 <rywls1110@naver.com>
2025-02-13 12:01:40 +0900

    ps

--- SWEA/5648-원자소멸시뮬레이션/main.cpp
+++ SWEA/5648-원자소멸시뮬레이션/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+#include <unodered_map>
 
 using namespace std;
 
@@ -16,15 +18,15 @@ struct Cell
     int y;
     int direction;
     int K; // 보유 에너지
-    bool isAlive;
+    bool isDead; // 죽었는지 
 };
 
 void Move(Cell* cell)
 {
-    if(cell.direction == UP) cell.y++;
-    if(cell.direction == DOWN) cell.y--;
-    if(cell.direction == LEFT) cell.x--;
-    if(cell.direction == RIGHT) cell.x++;
+    if(cell->direction == UP) cell->y++;
+    else if(cell->direction == DOWN) cell->y--;
+    else if(cell->direction == LEFT) cell->x--;
+    else if(cell->direction == RIGHT) cell->x++;
 }
 
 auto main() -> int
@@ -32,30 +34,28 @@ auto main() -> int
     freopen("sample_input.txt", "r", stdin);
 
     int T; cin >> T;
-    for(auto test_case=1; test_case<=T; test_case++)
+
+    for(int test_case=1; test_case<=T; test_case++)
     {
         int sum = 0;
         
         int N; cin >> N; // 원자들의 수 (1 <= N <= 1000)
         Cell cells[N];
+        memset(cells, 0, sizeof(cells));
+        
         for(auto i = 0; i < N; i++)
         {
             cin >> cells[i].x >> cells[i].y >> cells[i].direction >> cells[i].K;
-            cells[i].isAlive = true;
         }
         
-        // 종료조건?
-        while(true)
+        int timer = 0;
+        while(++timer)
         {
-            for(auto i = 0; i < N; i++)
-            {
-                Move(&cells[i]);
-            }
-
-            // 
+            
         }
         
         cout << '#' << test_case << ' ' << sum << endl;
     }
+    
     return 0;
 }
