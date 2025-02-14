d388c0287ad3b9306f751917266fca5cb0f164aa
rywls1110 <rywls1110@naver.com>
2025-02-14 17:30:42 +0900

    solve

--- A형기출/마법의물뿌리개/main.cpp
+++ A형기출/마법의물뿌리개/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 auto main() -> int
 {
+    freopen("input.txt", "r", stdin);
+
     int T; cin >> T;
     for(auto test_case=1; test_case<=T; test_case++)
     {
@@ -15,45 +17,77 @@ auto main() -> int
         for(auto i = 0; i < N; i++)
         {
             cin >> heights[i];
-            if(maxHeight < heights[i]) maxHeight = heights[i];
+            if(maxHeight < heights[i]) 
+            {
+                maxHeight = heights[i];
+            }
             sumHeight += heights[i];
         }
+        sort(heights, heights+N, greater<int>());
 
         int remainingSum = maxHeight*N - sumHeight;
-        
-        // 뺴야 될 수를 3으로 산정해서 2, 1 이런 식으로 빼면 될듯
-        // 2 부터 먼저 빼?
-        // 마지막에 순서 안맞으면 어떡하지
-        // 그니까 2를 빼면 무조건 1을 뺴고 가야됨
-        // 2를 못빼면 1을 빼고 하면 될듯
-        // 1만 남아있으면 무조건 1 먼저 빼는게 좋음
-        // 2만 남아있으면 참고 2 빼는게 좋음
-        // 총 합을 구해서 남아있는 값이 cntEven 값과 같을 때 까지 올려가야함.
-        // 마지막에 값 이상해지면, 홀수로 빼주면 돼 
 
         int day = 0;
         int cntOdd = 0;
         int cntEven = 0;
+
         while(remainingSum > 0)
         {
             ++day;
 
-            // 3 이상 남았을 때 => 1, 2 순서대로
-            // 2 남았을 때 0, 2
-            // 1 남았을 때 1, 0
-            
-
-            if(day%2 == 1)
-            {
-                
-            }
-            else
-            {
-                
+            for(auto i = 0; i < N; i++)
+            {   
+                int remaingHeight = maxHeight - heights[i];
+                if(remaingHeight == 0)
+                {
+                    continue;
+                }
+                else if(remaingHeight == 1)
+                {
+                    if(day%2==1)
+                    {
+                        heights[i] += 1;
+                        remainingSum -= 1;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if(remaingHeight == 2)
+                {
+                    if(day%2==0)
+                    {
+                        heights[i] += 2;
+                        remainingSum -= 2;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    } 
+                    
+                }
+                else if(remaingHeight > 2)
+                {
+                    if(day%2==0)
+                    {
+                        heights[i] += 2;
+                        remainingSum -= 2;
+                        break;
+                    }
+                    else
+                    {
+                        heights[i] += 1;
+                        remainingSum -= 1;
+                        break;
+                    }
+                }
             }
         }
         
-        cout << '#' << test_case << ' ' << day+remainingSum << endl;
+        cout << '#' << test_case << ' ' << day << endl;
     }
     return 0;
 }
