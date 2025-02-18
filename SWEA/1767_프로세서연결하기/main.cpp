9f467d5d68b6728a2bb2335574cbf13c778e0a3e
rywls1110 <rywls1110@naver.com>
2025-02-19 08:51:28 +0900

    ps

---
+++ SWEA/1767_프로세서연결하기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+auto main() -> int
+{
+    freopen("sample_input.txt", "r", stdin);
+
+    int T; cin >> T;
+    for(auto test_case=1; test_case<=T; test_case++)
+    {
+        int result = 0;
+        
+        
+        
+        cout << '#' << test_case << ' ' << result << endl;
+    }
+    return 0;
+}
