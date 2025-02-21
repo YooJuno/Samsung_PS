04ad260fa930eb73c71a26582c378a07cc005534
rywls1110 <rywls1110@naver.com>
2025-02-21 09:37:33 +0900

    ps

--- Algorithm/Graph/FloodFill.cpp
+++ Algorithm/Graph/FloodFill.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 /*
+
 [Flood Fill]
 - DFS 혹은 BFS를 사용하여 2차원 맵에서의 문제를 푸는 것
-- 보통은 BFS가 더 좋긴함.
+- 주변 방향으로 "퍼져나가는 방향" 이라면 BFS가 더 좋긴함.
+- 기존엔 int visited[] 였다면...
+    struct Pos 
+    {
+        int x;
+        int y;
+    };
+    bool VisitedX[];
+    bool visitedY[];
+    형식이 필요함
+
 */
 
 auto main() -> int
