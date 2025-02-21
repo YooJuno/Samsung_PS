1f968529be8c218c08d42f97042a5661224f6e95
rywls1110 <rywls1110@naver.com>
2025-02-21 10:31:38 +0900

    ps

---
+++ Algorithm/Simulation/Simulation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+[Simulation]
+- 피지컬 싸움임. 잘자고 잘먹고 잘풀면 됨.
+
+[필요 기술]
+- 방향 벡터
+- 케이스 및 예외 처리
+
+*/
+
+enum Direction
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+auto main() -> int
+{
+    return 0;
+}
